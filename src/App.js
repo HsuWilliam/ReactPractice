569d6507420ba60152fd52f52e2b5a09eb300e00
HsuWilliam <hsu870422@gmail.com>
2021-08-01 18:21:02 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import { BrowserRouter, Route, Link } from "react-router-dom";
+import React,{useState,useEffect} from "react";
 import './App.css';
 import { Component } from 'react';
 import Login from './pages/Login';
 import Home from './pages/Home';
 
+
 class App extends Component {
+
+  
   render(){
+    const {user,userstate} =useState('');
+    
     return(
     <BrowserRouter>
     {/* 設定路由，首頁為Home */}
